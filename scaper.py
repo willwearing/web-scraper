100fad6587b76c78f3df08a12ed5eb4b92e9ca93
willwearing <willwearing@gmail.com>
2022-03-29 15:29:24 -0600

    removed count_words func and added data set

--- scaper.py
+++ scaper.py
@@ -3,8 +3,7 @@ import ssl
 from bs4 import BeautifulSoup
 import urllib.request
 import mongoscript as mongo
-
-urls = ["https://trexity.com/", "https://www.fedex.com/en-ca/home.html"]
+import URL_DATA as urls
 
 headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.3'}
 
@@ -16,24 +15,24 @@ def fetchText(url):
     text = soup.get_text()
     return text
 
-def count_words(str):
-    counts = dict()
-    words = str.split()
+# def count_words(str):
+#     counts = dict()
+#     words = str.split()
 
-    for word in words:
-        if word in counts:
-            counts[word] += 1
-        else:
-            counts[word] = 1
+#     for word in words:
+#         if word in counts:
+#             counts[word] += 1
+#         else:
+#             counts[word] = 1
 
-    counts_sorted = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+#     counts_sorted = sorted(counts.items(), key=lambda x: x[1], reverse=True)
 
-    return counts_sorted
+#     return counts_sorted
 
 for url in urls:
     # fetch text for each url
     text = fetchText(url)
     #  count words in each set of 'text' 
-    word_tally = count_words(text)
+    # word_tally = count_words(text)
     # push url and word_tally to database
-    mongo.load_data(url, word_tally)
+    mongo.load_data(url, text)
